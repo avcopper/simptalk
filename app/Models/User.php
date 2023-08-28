200bdfdd91a4bf98994c15a5a413bfa7fa0114fe
andrew <avcopper@mail.ru>
2023-08-28 20:33:54 +0600

    model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,11 @@
 namespace Models;
 
 use DateTime;
-use System\Api;
 use System\Auth;
 use System\Db;
 use Exceptions\DbException;
 use Exceptions\UserException;
 use Models\User as ModelUser;
-use Models\UserSession as ModelUserSession;
 
 /**
  * Class User
@@ -18,7 +16,7 @@ class User extends Model
 {
     const MAX_COUNT_ATTEMPT = 5;
 
-    protected static $table = 'auth.users';
+    protected static $db_table = 'auth.users';
 
     public $id;
     public $active;
@@ -41,7 +39,7 @@ class User extends Model
     public $updated;
 
     /**
-     * Возвращает пользователя по id (+++)
+     * Возвращает пользователя по id (!+)
      */
     public static function getById(int $id, bool $active = true, $object = true)
     {
@@ -53,11 +51,11 @@ class User extends Model
                 u.id, u.active, u.blocked, ub.expire, u.group_id, ug.name group_name, u.login, u.password, u.pin, u.e_pin, u.email, u.phone, 
                 u.name,  u.second_name, u.last_name, u.gender_id, ugn.name gender, u.personal_data_agreement, u.mailing, 
                 u.mailing_type_id, tt.name mailing_type, u.created, u.updated
-            FROM " . self::$prefix . self::$table . " u 
-            LEFT JOIN " . self::$prefix . "auth.user_groups ug ON u.group_id = ug.id 
-            LEFT JOIN " . self::$prefix . "auth.user_genders ugn ON u.gender_id = ugn.id 
-            LEFT JOIN " . self::$prefix . "simptalk.text_types tt ON u.mailing_type_id = tt.id 
-            LEFT JOIN " . self::$prefix . "auth.user_blocks ub ON u.id = ub.user_id AND ub.expire > NOW() 
+            FROM " . self::$db_prefix . self::$db_table . " u 
+            LEFT JOIN " . self::$db_prefix . "auth.user_groups ug ON u.group_id = ug.id 
+            LEFT JOIN " . self::$db_prefix . "auth.user_genders ugn ON u.gender_id = ugn.id 
+            LEFT JOIN " . self::$db_prefix . "simptalk.text_types tt ON u.mailing_type_id = tt.id 
+            LEFT JOIN " . self::$db_prefix . "auth.user_blocks ub ON u.id = ub.user_id AND ub.expire > NOW() 
             WHERE u.id = :id {$activity}";
 
         $data = $db->query($object ? static::class : null);
@@ -65,7 +63,7 @@ class User extends Model
     }
 
     /**
-     * Возвращает пользователя по логину (+++)
+     * Возвращает пользователя по логину (!+)
      */
     public static function getByLogin(string $login, bool $active = true, $object = true)
     {
@@ -77,11 +75,11 @@ class User extends Model
                 u.id, u.active, u.blocked, u.group_id, ug.name group_name, u.login, u.password, u.pin, u.e_pin, u.email, u.phone, 
                 u.name,  u.second_name, u.last_name, u.gender_id, ugn.name gender, u.personal_data_agreement, u.mailing, 
                 u.mailing_type_id, tt.name mailing_type, u.created, u.updated
-            FROM " . self::$prefix . self::$table . " u 
-            LEFT JOIN " . self::$prefix . "auth.user_groups ug ON u.group_id = ug.id 
-            LEFT JOIN " . self::$prefix . "auth.user_genders ugn ON u.gender_id = ugn.id 
-            LEFT JOIN " . self::$prefix . "simptalk.text_types tt ON u.mailing_type_id = tt.id 
-            LEFT JOIN " . self::$prefix . "auth.user_blocks ub ON u.id = ub.user_id AND ub.expire > NOW() 
+            FROM " . self::$db_prefix . self::$db_table . " u 
+            LEFT JOIN " . self::$db_prefix . "auth.user_groups ug ON u.group_id = ug.id 
+            LEFT JOIN " . self::$db_prefix . "auth.user_genders ugn ON u.gender_id = ugn.id 
+            LEFT JOIN " . self::$db_prefix . "simptalk.text_types tt ON u.mailing_type_id = tt.id 
+            LEFT JOIN " . self::$db_prefix . "auth.user_blocks ub ON u.id = ub.user_id AND ub.expire > NOW() 
             WHERE u.login = :login {$activity}";
 
         $data = $db->query($object ? static::class : null);
@@ -89,7 +87,7 @@ class User extends Model
     }
 
     /**
-     * Возвращает пользователя по токену (+++)
+     * Возвращает пользователя по токену (!+)
      */
     public static function getByToken(?string $token, bool $active = true, bool $object = true)
     {
@@ -103,12 +101,12 @@ class User extends Model
                 u.id, u.active, u.blocked, u.group_id, ug.name group_name, u.login, u.password, u.pin, u.e_pin, u.email, u.phone, 
                 u.name,  u.second_name, u.last_name, u.gender_id, ugn.name gender, u.personal_data_agreement, u.mailing, 
                 u.mailing_type_id, tt.name mailing_type, u.created, u.updated
-            FROM " . self::$prefix . "auth.user_sessions  us 
-            LEFT JOIN " . self::$prefix . self::$table . " u ON us.login = u.login 
-            LEFT JOIN " . self::$prefix . "auth.user_groups ug ON u.group_id = ug.id 
-            LEFT JOIN " . self::$prefix . "auth.user_genders ugn ON u.gender_id = ugn.id 
-            LEFT JOIN " . self::$prefix . "simptalk.text_types tt ON u.mailing_type_id = tt.id 
-            LEFT JOIN " . self::$prefix . "auth.user_blocks ub ON u.id = ub.user_id AND ub.expire > NOW() 
+            FROM " . self::$db_prefix . "auth.user_sessions  us 
+            LEFT JOIN " . self::$db_prefix . self::$db_table . " u ON us.login = u.login 
+            LEFT JOIN " . self::$db_prefix . "auth.user_groups ug ON u.group_id = ug.id 
+            LEFT JOIN " . self::$db_prefix . "auth.user_genders ugn ON u.gender_id = ugn.id 
+            LEFT JOIN " . self::$db_prefix . "simptalk.text_types tt ON u.mailing_type_id = tt.id 
+            LEFT JOIN " . self::$db_prefix . "auth.user_blocks ub ON u.id = ub.user_id AND ub.expire > NOW() 
             WHERE us.token = :token {$activity}";
 
         $data = $db->query($object ? static::class : null);
@@ -116,19 +114,30 @@ class User extends Model
     }
 
     /**
-     * Получает текущего пользователя (+++)
+     * Возвращает текущего пользователя (!+)
      * @return mixed|null
-     * @throws UserException
      */
     public static function getCurrent()
     {
-        $token = defined('API') ? self::getRequestToken() : self::getUserToken();
-        return !empty($_SESSION['user']) ? $_SESSION['user'] : self::getByToken($token);
+        return !empty($_SESSION['user']) ? $_SESSION['user'] : self::getByToken(self::getUserToken());
     }
 
     /**
-     * Возвращает текущий сессионный токен (+++)
-     * @return mixed|null
+     * Проверяет авторизацию пользователя (!+)
+     * @return bool
+     */
+    public static function isAuthorized()
+    {
+        $token = self::getUserToken();
+        $device = self::getUserDevice();
+        if (empty($token) || empty($device)) return false;
+
+        return Auth::checkAuthorization($token, $device);
+    }
+
+    /**
+     * Возвращает текущий сессионный токен (!+)
+     * @return string|null
      */
     public static function getUserToken()
     {
@@ -136,116 +145,53 @@ class User extends Model
     }
 
     /**
-     * Возвращает авторизационный токен из тела запроса (+++)
-     * @return mixed
-     * @throws UserException
-     */
-    public static function getRequestToken()
-    {
-        if (empty($_SERVER['HTTP_AUTHORIZATION']) ||
-            !preg_match('/Bearer\s(\S+)/', $_SERVER['HTTP_AUTHORIZATION'], $matches) ||
-            empty($matches[1]))
-        {
-            throw new UserException(Auth::WRONG_TOKEN);
-        }
-
-        return $matches[1];
-    }
-
-    /**
-     * Возвращает данные пользовательского устройства (+++)
+     * Возвращает данные пользовательского устройства (!+)
      * @return array
      */
     public static function getUserDevice()
     {
         return [
-            'device' => base64_encode($_SERVER['HTTP_USER_AGENT']),
-            'ip' => base64_encode($_SERVER['REMOTE_ADDR']),
+            'device' => $_SERVER['HTTP_USER_AGENT'],
+            'ip' => $_SERVER['REMOTE_ADDR'],
             'serviceId' => 2
         ];
     }
 
     /**
-     * Возвращает данные пользовательского устройства из запроса к апи (+++)
-     * @return mixed
-     */
-    public static function getUserDeviceFromApi()
-    {
-        return json_decode(file_get_contents("php://input"), true);
-    }
-
-    /**
-     * Возвращает данные пользователя из запроса
-     * @param $userData
-     * @return array
-     */
-    public static function getUserFromApi($userData)
-    {
-        $user = explode(':', base64_decode($userData['user']));
-        return [
-            'login' => $user[0] ?? null,
-            'password' => $user[1] ?? null
-        ];
-    }
-
-    /**
-     * Проверяет авторизован ли пользователь (+++)
-     * @return bool
-     * @throws UserException
-     */
-    public static function isAuthorized()
-    {
-        return (new Api())->validate();
-    }
-
-    /**
-     * Авторизация (+++)
-     * @param string $login - логин
-     * @param string $password - пароль
-     * @param bool $remember - запомнить пользователя
-     * @param array $userData - пользовательское устройство
+     * Авторизация (!+)
      * @throws UserException|DbException
      */
-    public static function authorization(string $login, string $password, bool $remember = false, $userData = [])
+    public static function authorize($login, $password, $remember)
     {
         Auth::checkUser($login, $password);
-        Auth::checkDeviceData($userData);
 
         $auth = new Auth();
         $auth->user = \Entity\User::get(['login' => $login]);
-        $message = Auth::NOT_AUTHORIZED;
 
         if (!empty($auth->user->id)) { // найден активный пользователь
+            $auth->userSession = self::setUserSession($auth->user);
             $countFailedAttempts = UserSession::getCountFailedAttempts($login);
 
             if ($countFailedAttempts < ModelUser::MAX_COUNT_ATTEMPT) { // меньше 5 активных попыток входа
-                $auth->userSession = self::setUserSession($auth->user, $userData);
+                if (password_verify($password, $auth->user->password)) $auth->login($remember);
+                else {
+                    $auth->userSession->comment = Auth::WRONG_LOGIN_PASSWORD;
+                    $auth->userSession->save();
+                    throw new UserException(Auth::WRONG_LOGIN_PASSWORD, 401);
+                }
 
-                if (password_verify($password, $auth->user->ePin) && $userData['serviceId'] === UserSession::SERVICE_MOBILE)
-                    $auth->loginEmergencyPin();
-
-                elseif (password_verify($password, $auth->user->pin) && $userData['serviceId'] === UserSession::SERVICE_MOBILE &&
-                    Auth::checkAuthorization(User::getRequestToken(), $userData)) $auth->loginPin();
-
-                elseif (password_verify($password, $auth->user->password)) $auth->login($remember);
-
-                else $auth->userSession->comment = Auth::WRONG_LOGIN_PASSWORD;
-
-                $auth->userSession->save();
-            }
-            else {
-                $message = Auth::TOO_MANY_FAILED_ATTEMPTS;
+            } else {
                 UserSession::clearFailedAttempts($auth->user->login);
-                $auth->user->block(UserBlock::INTERVAL_DAY, $message);
+                $auth->user->block(UserBlock::INTERVAL_DAY, Auth::TOO_MANY_FAILED_ATTEMPTS);
+                throw new UserException(Auth::TOO_MANY_FAILED_ATTEMPTS, 401);
             }
         }
-        else $message = Auth::USER_NOT_FOUND;
 
-        throw new UserException($message, 401);
+        throw new UserException(Auth::USER_NOT_FOUND, 401);
     }
 
     /**
-     * Выход (+)
+     * Выход
      * @return bool
      */
     public static function logout(): bool
@@ -257,20 +203,19 @@ class User extends Model
     }
 
     /**
-     * Создает пользовательскую сессию (+++)
+     * Создает пользовательскую сессию (!+)
      * @param $user
-     * @param $userData
      * @return \Entity\UserSession
      */
-    protected static function setUserSession($user, $userData)
+    protected static function setUserSession($user)
     {
         $userSession = new \Entity\UserSession();
         $userSession->isActive = 1;
         $userSession->login = $user->login;
         $userSession->userId = $user->id;
-        $userSession->serviceId = $userData['serviceId'];
-        $userSession->ip = base64_decode($userData['ip']);
-        $userSession->device = base64_decode($userData['device']);
+        $userSession->serviceId = 2;
+        $userSession->ip = $_SERVER['REMOTE_ADDR'];
+        $userSession->device = $_SERVER['HTTP_USER_AGENT'];
         $userSession->logIn = new DateTime();
         $userSession->expire = null;
         $userSession->token = null;
@@ -278,118 +223,59 @@ class User extends Model
         return $userSession;
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public function filter_id($id)
-    {
-        return (int)$id;
-    }
-
-    public function filter_group_id($value)
-    {
-        return (int)$value;
-    }
-
-    public function filter_name($text)
-    {
-        return strip_tags(trim($text));
-    }
-
-    public function filter_email($text)
-    {
-        return strip_tags(trim($text));
-    }
-
-    public function filter_phone($value)
-    {
-        return (int)$value;
-    }
-
-    public function filter_password($text)
-    {
-        return strip_tags(trim($text));
-    }
+//    /**
+//     * Проверяет авторизован ли пользователь (+++)
+//     * @return bool
+//     * @throws UserException
+//     */
+//    public static function isAuthorizedByApi()
+//    {
+//        return (new Api())->validate();
+//    }
+
+//    /**
+//     * Авторизация (+++)
+//     * @param string $login - логин
+//     * @param string $password - пароль
+//     * @param bool $remember - запомнить пользователя
+//     * @param array $userData - пользовательское устройство
+//     * @throws UserException|DbException
+//     */
+//    public static function authorization(string $login, string $password, bool $remember = false, $userData = [])
+//    {
+//        Auth::checkUser($login, $password);
+//        Auth::checkDeviceData($userData);
+//
+//        $auth = new Auth();
+//        $auth->user = \Entity\User::get(['login' => $login]);
+//        $message = Auth::NOT_AUTHORIZED;
+//
+//        if (!empty($auth->user->id)) { // найден активный пользователь
+//            $countFailedAttempts = UserSession::getCountFailedAttempts($login);
+//
+//            if ($countFailedAttempts < ModelUser::MAX_COUNT_ATTEMPT) { // меньше 5 активных попыток входа
+//                $auth->userSession = self::setUserSession($auth->user, $userData);
+//
+//                if (password_verify($password, $auth->user->ePin) && $userData['serviceId'] === UserSession::SERVICE_MOBILE)
+//                    $auth->loginEmergencyPin();
+//
+//                elseif (password_verify($password, $auth->user->pin) && $userData['serviceId'] === UserSession::SERVICE_MOBILE &&
+//                    Auth::checkAuthorization(User::getRequestToken(), $userData)) $auth->loginPin();
+//
+//                elseif (password_verify($password, $auth->user->password)) $auth->login($remember);
+//
+//                else $auth->userSession->comment = Auth::WRONG_LOGIN_PASSWORD;
+//
+//                $auth->userSession->save();
+//            }
+//            else {
+//                $message = Auth::TOO_MANY_FAILED_ATTEMPTS;
+//                UserSession::clearFailedAttempts($auth->user->login);
+//                $auth->user->block(UserBlock::INTERVAL_DAY, $message);
+//            }
+//        }
+//        else $message = Auth::USER_NOT_FOUND;
+//
+//        throw new UserException($message, 401);
+//    }
 }
