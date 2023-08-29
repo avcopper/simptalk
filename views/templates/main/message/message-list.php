4f0fd596916b2b0e1814ce8bd6f35ef6c5ce30c7
andrew <avcopper@mail.ru>
2023-08-30 01:03:36 +0600

    view messages

--- views/templates/main/message/message-list.php
+++ views/templates/main/message/message-list.php
@@ -1,6 +1,15 @@
 <?php
-//$rsa_my = new System\OldRSA($user->private_key);
-//$rsa_friend = new System\OldRSA($friend->private_key);
+/**
+ * @var \System\Crypt $crypt
+ * @var \System\Crypt $cryptFriend
+ * @var \Entity\User $user
+ * @var \Entity\Friend $friend
+ * @var string $name
+ * @var string $lastName
+ * @var string $friendName
+ * @var string $friendLastName
+ */
+
 $date = '';
 $_monthsList = [
     1 => 'января',
@@ -19,32 +28,31 @@ $_monthsList = [
 
 if (!empty($messages) && is_array($messages)):
     foreach ($messages as $message):
-        $_dt = DateTime::createFromFormat('Y-m-d H:i:s', $message->created);
-        $time = $_dt->format('H:s');
+        $time = $message->created->format('H:s');
         $dt =
-            $_dt->format('d') . ' ' .
-            $_monthsList[$_dt->format('n')] .
-            ($_dt->format('Y') !== date('Y') ? (' ' . $_dt->format('Y')) : '');
+            $message->created->format('d') . ' ' .
+            $_monthsList[$message->created->format('n')] .
+            ($message->created->format('Y') !== date('Y') ? (' ' . $message->created->format('Y')) : '');
 
         if (!empty($showDate) && (empty($date) || $date !== $dt)): ?>
             <div class="message-date"><?= $dt; ?></div>
         <?php endif;
         $date = $dt; ?>
 
-        <div class="message-item <?= ($user->id === $message->message_from) ? 'my' : '' ?>" data-id="<?= $message->id ?>">
+        <div class="message-item <?= ($user->id === $message->messageFromUserId) ? 'my' : '' ?>" data-id="<?= $message->id ?>">
             <div class="message-photo">
                 <img src="/images/user.jpg" alt="">
             </div>
 
             <div class="message-name">
-                <?= $user->id === $message->message_from ? $rsa_my->decrypt($user->name) : $rsa_friend->decrypt($friend->name) ?>
+                <?= $user->id === $message->messageFromUserId ? "{$name} {$lastName}" : "{$friendName} {$friendLastName}" ?>
                 <span class="message-time"><?= $time ?></span>
             </div>
 
             <div class="message-body">
-                <?= $user->id === $message->message_from ?
-                    str_replace("\r\n", '<br>', $rsa_my->decrypt($message->message)) :
-                    str_replace("\r\n", '<br>', $rsa_friend->decrypt($message->message)) ?>
+                <?= $user->id === $message->messageFromUserId ?
+                    str_replace("\r\n", '<br>', $crypt->decryptByPublicKey($message->message)) :
+                    str_replace("\r\n", '<br>', $cryptFriend->decryptByPublicKey($message->message)) ?>
             </div>
         </div>
     <?php endforeach;
--- views/templates/main/message/message.php
+++ views/templates/main/message/message.php
@@ -1,12 +1,37 @@
 <?php
-//$rsa = new System\OldRSA($friend->private_key);
+/**
+ * @var \System\Crypt $crypt
+ * @var \System\Crypt $cryptFriend
+ * @var \Entity\User $user
+ * @var \Entity\Friend $friend
+ */
+
+$name = !empty($user->name) ? $crypt->decryptByPublicKey($user->name) : '';
+$secondName = !empty($user->secondName) ? $crypt->decryptByPublicKey($user->secondName) : '';
+$lastName = !empty($user->lastName) ? $crypt->decryptByPublicKey($user->lastName) : '';
+
+$friendName = !empty($friend->name) ? $cryptFriend->decryptByPublicKey($friend->name) : '';
+$friendSecondName = !empty($friend->secondName) ? $cryptFriend->decryptByPublicKey($friend->secondName) : '';
+$friendLastName = !empty($friend->lastName) ? $cryptFriend->decryptByPublicKey($friend->lastName) : '';
+
 ?>
 
 <div class="message">
-    <div class="message-title"><?//= $rsa_friend->decrypt($friend->name) . ' ' . $rsa_friend->decrypt($friend->last_name) ?></div>
+    <div class="message-title"><?= "{$friendName} {$friendSecondName} {$friendLastName}" ?></div>
 
     <div class="message-list" id="message-list">
-        <?= $this->render('message/message-list') ?>
+        <?= $this->render('message/message-list', [
+                'crypt' => $crypt,
+                'cryptFriend' => $cryptFriend,
+                'friendName' => $friendName,
+                'friendSecondName' => $friendSecondName,
+                'friendLastName' => $friendLastName,
+                'name' => $name,
+                'secondName' => $secondName,
+                'lastName' => $lastName,
+            ]
+        )
+        ?>
     </div>
 
     <div class="message-new">
@@ -23,7 +48,7 @@
         <div class="message-send"></div>
 
         <form action="">
-            <input type="hidden" name="message_to" value="<?//= $friend->id ?>">
+            <input type="hidden" name="message_to" value="<?= $friend->id ?>">
             <div class="message-text" tabindex="0" contenteditable="true" id="message-text" role="textbox" aria-multiline="true" ondragend="return true"></div>
         </form>
 
@@ -36,6 +61,7 @@
         let messageList = document.querySelector('.message-list'),
             messageText = $('#message-text'),
             messageTextHeight = messageText.height();
+
         messageList.scrollTop = messageList.scrollHeight;
 
         // вся эта байда для отслеживания размеров поля ввода сообщения
@@ -47,44 +73,44 @@
         });
 
         /* отправка ссобщения по нажатию кнопки */
-        $('.message-send').on('click', function (e) {
-            let data = {
-                    'friend_id': $('input[name=message_to]').val(),
-                    'last': $('#message-list .message-item').last().data('id'),
-                    'message': messageText.html(),
-                };
-            if(data.message.length) sendMessage(data);
-        });
+        // $('.message-send').on('click', function (e) {
+        //     let data = {
+        //             'friend_id': $('input[name=message_to]').val(),
+        //             'last': $('#message-list .message-item').last().data('id'),
+        //             'message': messageText.html(),
+        //         };
+        //     if(data.message.length) sendMessage(data);
+        // });
 
         /* отправка ссобщения по нажатию enter */
-        messageText.on('keydown', function (e) {
-            //if (e.ctrlKey && e.which === 13) message.append(document.createElement('br'));
-            if (e.ctrlKey && e.keyCode === 13) {
-                e.preventDefault();
-                let data = {
-                        'friend_id': $('input[name=message_to]').val(),
-                        'last': $('#message-list .message-item').last().data('id'),
-                        'message': messageText.html(),
-                    };
-                if(data.message.length) sendMessage(data);
-            }
-        });
+        // messageText.on('keydown', function (e) {
+        //     //if (e.ctrlKey && e.which === 13) message.append(document.createElement('br'));
+        //     if (e.ctrlKey && e.keyCode === 13) {
+        //         e.preventDefault();
+        //         let data = {
+        //                 'friend_id': $('input[name=message_to]').val(),
+        //                 'last': $('#message-list .message-item').last().data('id'),
+        //                 'message': messageText.html(),
+        //             };
+        //         if(data.message.length) sendMessage(data);
+        //     }
+        // });
 
-        function sendMessage(data) {
-            $.ajax({
-                method: "POST",
-                dataType: 'text',
-                url: '/messages/send/' + data.friend_id + '/',
-                data: data,
-                beforeSend: function() {
-                },
-                success: function(data){
-                    $('.message-text').html('');
-                    $('.message-list').append(data);
-                    messageList.scrollTop = messageList.scrollHeight;
-                }
-            });
-        }
+        // function sendMessage(data) {
+        //     $.ajax({
+        //         method: "POST",
+        //         dataType: 'text',
+        //         url: '/messages/send/' + data.friend_id + '/',
+        //         data: data,
+        //         beforeSend: function() {
+        //         },
+        //         success: function(data){
+        //             $('.message-text').html('');
+        //             $('.message-list').append(data);
+        //             messageList.scrollTop = messageList.scrollHeight;
+        //         }
+        //     });
+        // }
 
     //     $('.message-smiles img').on('click', function () {
     //         let img = ' <img src="'+ this.src +'">';
