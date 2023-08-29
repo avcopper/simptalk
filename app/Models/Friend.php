fb9c2b6455bc088c267ab5b2d1d3acdf2cecaa2e
andrew <avcopper@mail.ru>
2023-08-30 01:02:00 +0600

    model friend

--- app/Models/Friend.php
+++ app/Models/Friend.php
@@ -1,42 +1,69 @@
 <?php
-
 namespace Models;
 
 use System\Db;
 
+/**
+ * Class User
+ * @package App\Models
+ */
 class Friend extends Model
 {
-    protected static $db_table = 'friends';
+    protected static $db_table = 'auth.users';
 
-    public static function getAll(int $id, bool $active = true, $object = true)
+    public $id;
+    public $active;
+    public $blocked;
+    public $login;
+    public $name;
+    public $second_name;
+    public $last_name;
+    public $gender_id;
+
+    /**
+     * Возвращает пользователя по id (!+)
+     */
+    public static function getById(int $id, bool $active = true, $object = true)
     {
-        $activity =
-            !empty($active) ? 'AND u.active IS NOT NULL AND u.blocked IS NULL AND ug.active IS NOT NULL' : '';
-
-        $params = [':id' => $id];
-        $sql = "
+        $db = Db::getInstance();
+        $activity = !empty($active) ? 'AND u.active IS NOT NULL AND u.blocked IS NULL AND ub.expire IS NULL AND ug.active IS NOT NULL' : '';
+        $db->params = ['id' => $id];
+        $db->sql = "
             SELECT 
-                u.id, u.last_name, u.name, u.second_name,
-                f.status_id, 
-                s.name status_name, s.created request_date, 1 request_person 
-            FROM friends f 
-            LEFT JOIN users u ON u.id = f.user2_id 
-            LEFT JOIN statuses s ON s.id = f.status_id 
-            LEFT JOIN user_groups ug ON ug.id = u.group_id 
-            WHERE user1_id = :id {$activity} 
-            UNION ALL 
-            SELECT 
-                u.id, u.last_name, u.name, u.second_name,
-                f.status_id, 
-                s.name status_name, s.created request_date, 2 request_person 
-            FROM friends f 
-            LEFT JOIN users u ON u.id = f.user1_id 
-            LEFT JOIN statuses s ON s.id = f.status_id 
-            LEFT JOIN user_groups ug ON ug.id = u.group_id 
-            WHERE user2_id = :id {$activity}";
+                u.id, u.active, u.blocked, ub.expire, u.login, 
+                u.name,  u.second_name, u.last_name, u.gender_id, ugn.name gender 
+            FROM " . self::$db_prefix . self::$db_table . " u 
+            LEFT JOIN " . self::$db_prefix . "auth.user_groups ug ON u.group_id = ug.id 
+            LEFT JOIN " . self::$db_prefix . "auth.user_genders ugn ON u.gender_id = ugn.id 
+            LEFT JOIN " . self::$db_prefix . "simptalk.text_types tt ON u.mailing_type_id = tt.id 
+            LEFT JOIN " . self::$db_prefix . "auth.user_blocks ub ON u.id = ub.user_id AND ub.expire > NOW() 
+            WHERE u.id = :id {$activity}";
 
-        $db = new Db();
-        $data = $db->query($sql, $params, $object ? static::class : null);
-        return $data ?? false;
+        $data = $db->query($object ? static::class : null);
+        return !empty($data) ? array_shift($data) : null;
+    }
+
+    /**
+     * Возвращает пользователя по логину (!+)
+     */
+    public static function getByLogin(string $login, bool $active = true, $object = true)
+    {
+        $db = Db::getInstance();
+        $activity = !empty($active) ? 'AND u.active IS NOT NULL AND u.blocked IS NULL AND ub.expire IS NULL AND ug.active IS NOT NULL' : '';
+        $db->params = ['login' => $login];
+        $db->sql = "
+            SELECT 
+                u.id, u.active, u.blocked, u.group_id, ug.name group_name, u.login, u.password, u.pin, u.e_pin, u.email, u.phone, 
+                u.name,  u.second_name, u.last_name, u.gender_id, ugn.name gender, u.personal_data_agreement, u.mailing, 
+                u.mailing_type_id, tt.name mailing_type, u.created, u.updated
+            FROM " . self::$db_prefix . self::$db_table . " u 
+            LEFT JOIN " . self::$db_prefix . "auth.user_groups ug ON u.group_id = ug.id 
+            LEFT JOIN " . self::$db_prefix . "auth.user_genders ugn ON u.gender_id = ugn.id 
+            LEFT JOIN " . self::$db_prefix . "simptalk.text_types tt ON u.mailing_type_id = tt.id 
+            LEFT JOIN " . self::$db_prefix . "auth.user_blocks ub ON u.id = ub.user_id AND ub.expire > NOW() 
+            WHERE u.login = :login {$activity}";
+
+        $data = $db->query($object ? static::class : null);
+        return !empty($data) ? array_shift($data) : null;
     }
 }
