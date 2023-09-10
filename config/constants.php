604b138d59c7c8525ac077522716a601c894b9d0
andrew <avcopper@mail.ru>
2023-09-10 16:22:24 +0600

    constants

--- config/constants.php
+++ config/constants.php
@@ -13,7 +13,6 @@ const DIR_PUBLIC = __DIR__ . '/../public';
 const DIR_VIEWS = __DIR__ . '/../views';
 const DIR_CERTIFICATES = __DIR__ . '/../certificates';
 const DIR_TEMPLATES = __DIR__ . '/../views/templates';
-
 define("CONFIG", Config::getInstance()->data);
 
 if (empty($_SESSION['protocol']) || empty($_SESSION['domain']) || empty($_SESSION['api_protocol']) ||
@@ -24,18 +23,14 @@ if (empty($_SESSION['protocol']) || empty($_SESSION['domain']) || empty($_SESSIO
 
         $_SESSION['protocol'] = $settings['protocol'] ?: null;
         $_SESSION['domain'] = $settings['domain'] ?: null;
-
         $_SESSION['api_protocol'] = $settings['api_protocol'] ?: null;
         $_SESSION['api_domain'] = $settings['api_domain'] ?: null;
         $_SESSION['api_version'] = $settings['api_version'] ?: null;
-
         $_SESSION['sitename'] = $settings['sitename'] ?: null;
         $_SESSION['slogan'] = $settings['slogan'] ?: null;
-
         $_SESSION['template'] = $settings['template'] ?: null;
         $_SESSION['charset'] = $settings['charset'] ?: null;
         $_SESSION['auth_days'] = $settings['auth_days'] ?: null;
-
         $_SESSION['email'] = $settings['email'] ?: null;
         $_SESSION['tg'] = $settings['tg'] ?: null;
     }
