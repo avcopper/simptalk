d6c73dfe639ef86c232dbe83034aba416250ff3f
andrew <avcopper@mail.ru>
2023-09-30 00:27:21 +0600

    index

--- public/index.php
+++ public/index.php
@@ -10,10 +10,6 @@ use System\Request;
 use System\Response;
 use System\Security;
 use Controllers\Errors;
-use Exceptions\DbException;
-use Exceptions\UserException;
-use Exceptions\NotFoundException;
-use Exceptions\ForbiddenException;
 
 Security::array_xss_clean($_GET);
 Security::array_xss_clean($_POST);
@@ -22,19 +18,10 @@ Route::parseUrl($_SERVER['REQUEST_URI']);
 
 try {
     Route::start();
-} catch (DbException $e) {
-    if (Request::isAjax()) Response::result(false, $e->getMessage());
-    else (new Errors($e))->action('action500');
-} catch (NotFoundException $e) {
-    if (Request::isAjax()) Response::result(false, $e->getMessage());
-    else (new Errors($e))->action('action404');
-} catch (ForbiddenException $e) {
-    if (Request::isAjax()) Response::result(false, $e->getMessage());
-    else (new Errors($e))->action('action403');
-} catch (UserException $e) {
-    if (Request::isAjax()) Response::result(false, $e->getMessage());
-    else (new Errors($e))->action('action400');
 } catch (Exception $e) {
-    if (Request::isAjax()) Response::result(false, $e->getMessage());
-    else (new Errors($e))->action('action500');
+    if (Request::isAjax()) Response::result($e->getCode(), false, $e->getMessage());
+    else (new Errors($e))->action('actionError', $e->getCode());
+} catch(TypeError $e) {
+    if (Request::isAjax()) Response::result($e->getCode(), false, $e->getMessage());
+    else (new Errors($e))->action('actionError', $e->getCode());
 }
