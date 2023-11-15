7ae0e53139ec9ee743c19c8d13a8e5659c1122e1
andrew <andreycopper@gmail.com>
2023-11-16 00:22:42 +0600

    route

--- app/System/Route.php
+++ app/System/Route.php
@@ -20,25 +20,14 @@ class Route
         $uri = mb_substr(trim($uri), 1, mb_strlen($uri) - 2);
         $parts  = explode('/', $uri);
         $routes = [];
-        $urls = [];
-        $link = '';
 
         foreach ($parts as $part) {
             $elem = ucfirst(str_replace('-', '_', $part));
-            $link .= ($link ? '/' : '') . str_replace('-', '_', $part);
 
-            if (!empty($elem)) {
-                $routes[] = $elem;
-
-                $urls[] = [
-                    'name' => $elem,
-                    'link' => $link
-                ];
-            }
+            if (!empty($elem)) $routes[] = $elem;
         }
 
-        define('ROUTE', $routes); // ['Catalog', 'Conditioners', 'Mobile', '335']
-        define('URL', $urls); // [['name' => 'Personal', 'link' => 'personal'], ['name' => 'Subscriptions', 'link' => 'personal/subscriptions']]
+        define('ROUTE', array_values($routes)); // ['Catalog', 'Conditioners', 'Mobile', '335']
     }
 
     /**
@@ -51,153 +40,72 @@ class Route
      */
     public static function start()
     {
-        $class  = null;            // класс контроллера
-        $action = null;            // метод контроллера
-        $param1  = null;          // параметр1 метода
-        $param2  = null;          // параметр2 метода
-        $route  = ROUTE;         // массив роутов
+        $routes  = ROUTE;// массив роутов
+        $controller = 'Controllers';
+        $results = [];
+        $params = [];
 
-        $base = 'Controllers';
-        switch (count($route)) {
-            case 4:
-                if (class_exists($base . '\\' . $route[0] . '\\' . $route[1] . '\\' . $route[2] . '\\' . $route[3] . '\\' . 'Index') &&
-                    method_exists($base . '\\' . $route[0] . '\\' . $route[1] . '\\' . $route[2] . '\\' . $route[3] . '\\' . 'Index', 'actionDefault'))
-                {
-                    $class = $base . '\\' . $route[0] . '\\' . $route[1] . '\\' . $route[2] . '\\' . $route[3] . '\\' . 'Index';
-                    $action = 'actionDefault';
+        foreach ($routes as $i => $route) {
+            if (is_numeric($route)) {
+                $params[] = $route;
+                continue;
+            }
+
+            if ($i >= count($routes) - 3) {
+                if (class_exists($controller) && method_exists($controller, 'action' . $route)) {
+                    $params1 = [];
+                    if (!empty($routes[$i + 1])) $params1[] = $routes[$i + 1];
+                    if (!empty($routes[$i + 2])) $params1[] = $routes[$i + 2];
+                    $results[] = ['class' => $controller, 'method' => 'action' . $route, 'params' => array_merge($params, $params1)];
                 }
-                elseif (class_exists($base . '\\' . $route[0] . '\\' . $route[1] . '\\' . $route[2] . '\\' . $route[3]) &&
-                    method_exists($base . '\\' . $route[0] . '\\' . $route[1] . '\\' . $route[2] . '\\' . $route[3], 'actionDefault'))
-                {
-                    $class = $base . '\\' . $route[0] . '\\' . $route[1] . '\\' . $route[2] . '\\' . $route[3];
-                    $action = 'actionDefault';
+
+                if (empty($routes[$i + 1]) && empty($routes[$i + 2])) {
+                    if (class_exists($controller . ('\\' . $route)) && method_exists($controller . ('\\' . $route), 'actionDefault')) {
+                        $results[] = ['class' => $controller . ('\\' . $route), 'method' => 'actionDefault'];
+                    }
                 }
-                elseif (class_exists($base . '\\' . $route[0] . '\\' . $route[1] . '\\' . $route[2]) &&
-                    method_exists($base . '\\' . $route[0] . '\\' . $route[1] . '\\' . $route[2], "action{$route[3]}"))
-                {
-                    $class = $base . '\\' . $route[0] . '\\' . $route[1] . '\\' . $route[2];
-                    $action = "action{$route[3]}";
+
+                if (class_exists($controller . ('\\Index')) && method_exists($controller . ('\\Index'), 'action' . $route)) {
+                    $params1 = [];
+                    if (!empty($routes[$i + 1])) $params1[] = $routes[$i + 1];
+                    if (!empty($routes[$i + 2])) $params1[] = $routes[$i + 2];
+                    $results[] = ['class' => $controller . ('\\Index'), 'method' => 'action' . $route, 'params' => array_merge($params, $params1)];
                 }
-                elseif (class_exists($base . '\\' . $route[0] . '\\' . $route[1] . '\\' . $route[2]) &&
-                    method_exists($base . '\\' . $route[0] . '\\' . $route[1] . '\\' . $route[2], "actionShow"))
-                {
-                    $class = $base . '\\' . $route[0] . '\\' . $route[1] . '\\' . $route[2];
-                    $action = "actionShow";
-                    $param1 = $route[3];
+
+                if (!empty($routes[$i + 1]) && empty($routes[$i + 2])) {
+                    if (class_exists($controller . ('\\' . $route)) && method_exists($controller . ('\\' . $route), 'actionShow')) {
+                        $params1 = [];
+                        if (!empty($routes[$i + 1])) $params1[] = $routes[$i + 1];
+                        $results[] = ['class' => $controller . ('\\' . $route), 'method' => 'actionShow', 'params' => array_merge($params, $params1)];
+                    }
+
+                    if (class_exists($controller . ('\\' . $route) . ('\\Index')) && method_exists($controller . ('\\' . $route) . ('\\Index') , 'actionShow')) {
+                        $params1 = [];
+                        if (!empty($routes[$i + 1])) $params1[] = $routes[$i + 1];
+                        $results[] = ['class' => $controller . ('\\' . $route) . ('\\Index'), 'method' => 'actionShow', 'params' => array_merge($params, $params1)];
+                    }
+
                 }
-                elseif (class_exists($base . '\\' . $route[0] . '\\' . $route[1]) &&
-                    method_exists($base . '\\' . $route[0] . '\\' . $route[1], "action{$route[2]}"))
-                {
-                    $class = $base . '\\' . $route[0] . '\\' . $route[1];
-                    $action = "action{$route[2]}";
-                    $param1 = $route[3];
-                }
-                elseif (class_exists($base . '\\' . $route[0]) &&
-                    method_exists($base . '\\' . $route[0], "action{$route[1]}"))
-                {
-                    $class = $base . '\\' . $route[0];
-                    $action = "action{$route[1]}";
-                    $param1 = $route[2];
-                    $param2 = $route[3];
-                }
-                break;
-            case 3:
-                if (class_exists($base . '\\' . $route[0] . '\\' . $route[1] . '\\' . $route[2] . '\\' . 'Index') &&
-                    method_exists($base . '\\' . $route[0] . '\\' . $route[1] . '\\' . $route[2] . '\\' . 'Index', "actionDefault"))
-                {
-                    $class = $base . '\\' . $route[0] . '\\' . $route[1] . '\\' . $route[2] . '\\' . 'Index';
-                    $action = "actionDefault";
-                }
-                elseif (class_exists($base . '\\' . $route[0] . '\\' . $route[1] . '\\' . $route[2]) &&
-                    method_exists($base . '\\' . $route[0] . '\\' . $route[1] . '\\' . $route[2], "actionDefault"))
-                {
-                    $class = $base . '\\' . $route[0] . '\\' . $route[1] . '\\' . $route[2];
-                    $action = "actionDefault";
-                }
-                elseif (class_exists($base . '\\' . $route[0] . '\\' . $route[1]) &&
-                    method_exists($base . '\\' . $route[0] . '\\' . $route[1], "action{$route[2]}"))
-                {
-                    $class = $base . '\\' . $route[0] . '\\' . $route[1];
-                    $action = "action{$route[2]}";
-                }
-                elseif (class_exists($base . '\\' . $route[0] . '\\' . $route[1]) &&
-                    method_exists($base . '\\' . $route[0] . '\\' . $route[1], "actionShow"))
-                {
-                    $class = $base . '\\' . $route[0] . '\\' . $route[1];
-                    $action = "actionShow";
-                    $param1 = $route[2];
-                }
-                elseif (class_exists($base . '\\' . $route[0]) &&
-                    method_exists($base . '\\' . $route[0], "action{$route[1]}"))
-                {
-                    $class = $base . '\\' . $route[0];
-                    $action = "action{$route[1]}";
-                    $param1 = $route[2];
-                }
-                elseif (class_exists($base . '\\' . $route[0]) &&
-                    method_exists($base . '\\' . $route[0], "actionShow"))
-                {
-                    $class = $base . '\\' . $route[0];
-                    $action = "actionShow";
-                    $param1 = $route[1];
-                    $param2 = $route[2];
-                }
-                break;
-            case 2:
-                if (class_exists($base . '\\' . $route[0] . '\\' . $route[1] . '\\' . 'Index') &&
-                    method_exists($base . '\\' . $route[0] . '\\' . $route[1] . 'Index', "actionDefault"))
-                {
-                    $class = $base . '\\' . $route[0] . '\\' . $route[1] . '\\' . 'Index';
-                    $action = "actionDefault";
-                }
-                elseif (class_exists($base . '\\' . $route[0] . '\\' . $route[1]) &&
-                    method_exists($base . '\\' . $route[0] . '\\' . $route[1], "actionDefault"))
-                {
-                    $class = $base . '\\' . $route[0] . '\\' . $route[1];
-                    $action = "actionDefault";
-                }
-                elseif (class_exists($base . '\\' . $route[0]) &&
-                    method_exists($base . '\\' . $route[0], "action{$route[1]}"))
-                {
-                    $class = $base . '\\' . $route[0];
-                    $action = "action{$route[1]}";
-                }
-                elseif (class_exists($base . '\\' . $route[0]) &&
-                    method_exists($base . '\\' . $route[0], "actionShow"))
-                {
-                    $class = $base . '\\' . $route[0];
-                    $action = "actionShow";
-                    $param1 = $route[1];
-                }
-                break;
-            case 1:
-                if (class_exists($base . '\\' . $route[0] . '\\' . 'Index') &&
-                    method_exists($base . '\\' . $route[0] . 'Index', "actionDefault"))
-                {
-                    $class = $base . '\\' . $route[0] . '\\' . 'Index';
-                    $action = "actionDefault";
-                }
-                elseif (class_exists($base . '\\' . $route[0]) &&
-                    method_exists($base . '\\' . $route[0], "actionDefault"))
-                {
-                    $class = $base . '\\' . $route[0];
-                    $action = "actionDefault";
-                }
-                break;
-            case 0:
-                $class = $base . '\\Index';
-                $action = "actionDefault";
-                break;
+            }
+
+            $controller .= ('\\' . $route);
         }
 
-        if (!empty($class) && !empty($action)) {
-            $controller = new $class;
+        if (!empty($results) && is_array($results)) {
+            $routeInfo = array_pop($results);
 
-            if (!empty($param1) && !empty($param2)) $controller->action($action, mb_strtolower($param1), mb_strtolower($param2));
-            elseif (!empty($param1)) $controller->action($action, mb_strtolower($param1));
-            else $controller->action($action);
+            if (!empty($routeInfo['class']) && !empty($routeInfo['method'])) {
+                $class = $routeInfo['class'];
+                $method = $routeInfo['method'];
 
-        } else
-            if (!in_array('Js', ROUTE)) throw new NotFoundException(); // inputmask какого-то хрена далет еще один запрос...
+                $controller = new $class;
+
+                if (!empty($params[0]) && !empty($params[1])) $controller->action($method, mb_strtolower($params[0]), mb_strtolower($params[1]));
+                elseif (!empty($params[0])) $controller->action($method, mb_strtolower($params[0]));
+                else $controller->action($method);
+            }
+        }
+
+        throw new NotFoundException();
     }
 }
