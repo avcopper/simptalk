a8275e77927661261665170c555f2e527fa769ba
andrew <andreycopper@gmail.com>
2023-11-17 21:09:19 +0600

    config

--- config/config.php
+++ config/config.php
@@ -9,9 +9,9 @@ return [
         'password' => 'root'
     ],
     'log' => [
-        'error'   => DIR_LOGS . DIRECTORY_SEPARATOR . 'error' . DIRECTORY_SEPARATOR . date('Y-m-d') . '.log',
-        'access'  => DIR_LOGS . DIRECTORY_SEPARATOR . 'access' . DIRECTORY_SEPARATOR . date('Y-m-d') . '.log',
-        'system'  => DIR_LOGS . DIRECTORY_SEPARATOR . 'system' . DIRECTORY_SEPARATOR . date('Y-m-d') . '.log',
-        'warning' => DIR_LOGS . DIRECTORY_SEPARATOR . 'warning' . DIRECTORY_SEPARATOR . date('Y-m-d') . '.log',
+        'error'   => date('d') . '.log',
+        'access'  => date('d') . '.log',
+        'system'  => date('d') . '.log',
+        'warning' => date('d') . '.log',
     ]
 ];
