eff01de22191f4650e7c5942be3dca4ad3276029
andrew <andreycopper@gmail.com>
2023-11-18 23:55:35 +0600

    view

---
+++ views/templates/simple/lock.php
@@ -0,0 +1,24 @@
+<div class="py-4">
+
+    <div class="text-center mb-5">
+        <h3>Lock screen</h3>
+        <p class="text-muted">Enter your password to unlock the screen!</p>
+    </div>
+    <div class="user-thumb text-center mb-4">
+        <img src="/images/avatar-1.jpg" class="rounded-circle img-thumbnail avatar-lg" alt="thumbnail">
+        <h5 class="font-size-15 mt-3">Kathryn Swarey</h5>
+    </div>
+    <form>
+        <div class="mb-3">
+            <label class="form-label" for="userpassword">Password</label>
+            <input type="password" class="form-control" id="userpassword" placeholder="Enter password">
+        </div>
+        <div class="mb-3 mt-4">
+            <button class="btn btn-primary w-100 waves-effect waves-light" type="submit">Unlock</button>
+        </div>
+    </form><!-- end form -->
+
+    <div class="mt-5 text-center text-muted">
+        <p>Not you ? return <a href="/auth/" class="fw-medium text-decoration-underline"> Login</a></p>
+    </div>
+</div>
